d30817ec728debf85706645c6ab9420e748aa6a8
Josh_corgi <whvkek@naver.com>
2024-05-06 22:38:48 +0900

    5/6 학습마무리, 회원가입

--- src/main/java/com/ssamz/web/user/InsertUserServlet.java
+++ src/main/java/com/ssamz/web/user/InsertUserServlet.java
@@ -7,35 +7,20 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import com.ssamz.biz.user.UserDAO;
+import com.ssamz.biz.user.UserVO;
+
 /**
  * Servlet implementation class InsertUserServlet
  */
 public class InsertUserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public InsertUserServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
-		
 		//1. 사용자 입력 정보 추출
 		String id = request.getParameter("id");
 		String password = request.getParameter("password");
@@ -44,13 +29,33 @@ public class InsertUserServlet extends HttpServlet {
 		String selfInfo = request.getParameter("selfInfo");
 		String[] languages = request.getParameterValues("languages");
 		String age = request.getParameter("age");
-		System.out.println("아이디 : "+id);
-		System.out.println("비밀번호 : "+password);
-		System.out.println("이름 : "+name);
-		System.out.println("권한 : "+role);
-		System.out.println("자기 소개 : "+selfInfo);
-		System.out.println("언어 경험 : "+languages);
-		System.out.println("나이 : "+age);
+		
+		//2. DB 연동 처리
+		UserVO vo = new UserVO();
+		vo.setId(id);
+		vo.setPassword(password);
+		vo.setName(name);
+		vo.setRole(role);
+		
+		UserDAO dao = new UserDAO();
+		dao.insertUser(vo);
+		
+		//3. 화면 이동
+		//sendRedirect(String redirectPage) 메소드는 응답받은 브라우저가 다시 한번 redirectPage로 지정한 요청('login.html')을 서버에 전달하도록 한다.
+		response.sendRedirect("login.html");
+		
+		
+		System.out.println("아이디 : " + id);
+		System.out.println("비밀번호 : " + password);
+		System.out.println("이름 : " + name);
+		System.out.println("권한 : " + role);
+		System.out.println("자기 소개 : " + selfInfo);
+		System.out.println("언어 경험");
+		for(String language : languages) {
+			System.out.print(language + ", ");
+		}
+		System.out.println();
+		System.out.println("나이 : " + age);
 	}
 
 }
