c38f1ae23fc0f788774dd228af36f0685cd1ba33
sungrae <sungrae@DESKTOP-OUJ4159>
2024-05-09 18:17:22 +0900

    재설치 후 테스트

--- src/main/java/com/ssamz/biz/user/UserVO.java
+++ src/main/java/com/ssamz/biz/user/UserVO.java
@@ -2,6 +2,7 @@ package com.ssamz.biz.user;
 
 import lombok.Data;
 
+
 @Data
 //메소드 호출시 호출되는 메소드의 매개변수 개수와 타입만 일치하면 실행이되어버리기때문에 VO 클래스를 작성하여 여러개의 매개변수를 사용하는 메소드에 대해서 매개변수를 하나로 통합하는 역할.
 public class UserVO {
